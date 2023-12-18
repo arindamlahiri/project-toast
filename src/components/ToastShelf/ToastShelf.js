5663d07c325e25777467d3df2a2c74ebfa220f84
Arindam Lahiri <arindamlahiri17@gmail.com>
2023-12-18 20:48:30 +0530

    exercise 5.2 - updates for screen reader users

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -21,7 +21,12 @@ function ToastShelf() {
 	}, [dismissAllToasts]);
 
 	return (
-		<ol className={styles.wrapper}>
+		<ol
+			className={styles.wrapper}
+			role="region"
+			aria-live="polite"
+			aria-label="Notification"
+		>
 			{toastMessages.map(({ id, message, variant }) => (
 				<li key={id} className={styles.toastWrapper}>
 					<Toast variant={variant} onClose={() => onToastDismiss(id)}>
